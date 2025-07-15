339b7045977ca26475fcb9f414837c0336601b44
Aditya Jain <adityajain77356@gmail.com>
2025-07-15 21:40:37 +0530

    custom publish action feature added to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,43 @@
 from django.contrib import admin
 from .admin_site import blog_site_admin
-from .models import Post
+from .models import Post,Category , Comment
 
 
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-blog_site_admin.register(Post,BlogAdmin)
+def publish_action(modeladmin,request,queryset):
+    queryset.update(status='PUBLISHED')
+publish_action.short_description = "Mark selected items as published"
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','description' , 'rating','status')
+    list_filter = ['rating']
+    fields = ['title','description','status']
+    actions = [publish_action]
+blog_site_admin.register(Post,PostAdmin)
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name','count']
+    list_display = ('name','count' ,)
+    list_filter = ("name",)
+    
+    class Meta:
+        
+        managed = True
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['-created_at']
+    
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name','comment_desc','created_at')
+    list_filter = ['user_name']
+    
+    class  Meta:
+        
+        managed = True
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        ordering = ['-created_at']
+    
+blog_site_admin.register(Category, CategoryAdmin)
+blog_site_admin.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,40 @@
 from django.db import models
-
-# Create your models here.
-
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 class Post(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('PUBLISHED', 'Published'),
+        ('UNPUBLISHED', 'Unpublished'),
+    ]
     title = models.CharField(max_length=200)
+    description = models.TextField(max_length=250)
+    rating = models.IntegerField(
+        validators=[MaxValueValidator(5), MinValueValidator(0)])
+    status = models.CharField(choices=STATUS_CHOICES,default='PENDING',)
+    category = models.ForeignKey('Category', verbose_name=_(
+        "category_name"), on_delete=models.PROTECT, default=1)
+    created_at = models.DateTimeField(default=timezone.now)
+    
     
     def __str__(self):
         return self.title
+    
+class Category(models.Model):
+    # options
+    name = models.CharField(max_length=200)
+    count = models.IntegerField()
+        
+    def __str__(self):
+        return  self.name
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=199)
+    comment_desc = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey("Post", verbose_name=_("post_comment"), on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.user_name
